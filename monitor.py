31e5b02ccdcefe906735a6a1fed6d97806b7d1d1
Jimmy Bourque <jimmybchopps@gmail.com>
2023-07-14 08:14:36 -0500

    Added basic logging

--- monitor.py
+++ monitor.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import configparser
+import logging
 import requests
 
 config = configparser.ConfigParser()
@@ -12,11 +13,20 @@ mongo_db = config.get('Settings', 'MONGO_DB')
 mongo_collection = config.get('Settings', 'MONGO_COLLECTION')
 mongo_operations = config.get('Settings', 'OPERATIONS_TO_MONITOR')
 
+# Set up Logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler('change-stream.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
+# Set up connection to MongoDB
 client = MongoClient(mongo_connection)
 db = client[mongo_db]
 collection = db[mongo_collection]
 
+# Set up API Calls
 update_api = solr_url + '/update/json/docs'
 
 # Create a change stream
@@ -26,10 +36,9 @@ change_stream = collection.watch()
 for change in change_stream:
     if change['operationType'] == 'insert':
         new_document = change['fullDocument']
-        # Post to API
+        # Post to API and log
         response = requests.post(update_api, json=new_document)
-
-        # Error handling for the API request would go here
-        print(response.status_code)
+        logger.info('Added document into solr core: {}'.format(new_document))
     else:
-        print(change['operationType'] + " does not require any updates")
+        logger.exception('Failed to add document to solr core: {}'.format(
+            new_document), exc_info=True)
